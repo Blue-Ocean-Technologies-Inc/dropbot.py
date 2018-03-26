989d0f3af242c7f105e5f658dab406eb0068f216
Christian Fobel <christian@fobel.net>
2018-03-26 17:57:33 -0400

    feat(upload): retry fw upload (Ctrl-C to abort)
    Loop calls to `teensy_reboot.exe` to keep retrying firmware upload until
    Ctrl-C is pressed.
    
    See [sci-bots/platformio-helpers#7][pio-helpers-7]
    
    [pio-helpers-7]: https://github.com/sci-bots/platformio-helpers/issues/7

--- dropbot/bin/upload.py
+++ dropbot/bin/upload.py
@@ -1,8 +1,13 @@
-from __future__ import absolute_import
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+import itertools as it
 import platform
 import subprocess
+import sys
+import threading
 
 from platformio_helpers.upload import upload_conda
+import colorama as co
 import conda_helpers as ch
 
 
@@ -11,6 +16,9 @@ def upload():
     .. versionchanged:: 1.29
         Add ``dropbot-upload`` entry point (i.e., make script callable as
         ``dropbot-upload`` from system command shell).
+    .. versionchanged:: X.X.X
+        If upload fails, leave Teensy GUI open for 10 seconds to allow manual
+        reset into programming mode.
     '''
     if platform.system() == 'Windows':
         # Upload using Teensy GUI to allow auto-reboot on Windows.
@@ -20,13 +28,78 @@ def upload():
         # [i19]: https://gitlab.com/sci-bots/dropbot.py/issues/19
 
         # Launch Teensy graphical uploader program.
-        teensy_exe_path = (ch.conda_prefix() / 'share' / 'platformio' /
-                           'packages' / 'tool-teensy' / 'teensy.exe')
+        teensy_tools_dir = (ch.conda_prefix() / 'share' / 'platformio' /
+                            'packages' / 'tool-teensy')
+        teensy_exe_path = teensy_tools_dir.joinpath('teensy.exe')
         p = subprocess.Popen([teensy_exe_path])
-        # Trigger PlatformIO upload command, which will use graphical uploader that
-        # is already running.
+
+        def on_error(exception):
+            # Loop calls to `teensy_reboot.exe` to keep retrying firmware
+            # upload until Ctrl-C is pressed.
+            #
+            # XXX The `teensy_reboot.exe` process hangs if the Teensy Loader
+            # GUI window is closed while the `teensy_reboot.exe` process is
+            # waiting to detect the Teensy rebooting into programming mode.
+            # Otherwise, we could also cancel the upload by detecting when the
+            # GUI window is closed (i.e., `p.poll() is not None`).
+
+            # Upload failed
+            waiting_indicator = it.cycle(r'\|/-')
+
+            reboot_done = threading.Event()
+            status = threading.Event()
+            wait_complete = threading.Event()
+
+            def wait_for_reboot():
+                # Update no faster than `stderr` flush interval (if set).
+                update_interval = 2 * getattr(sys.stderr, 'flush_interval', .2)
+
+                message = (co.Fore.MAGENTA + 'Waiting for Teensy program '
+                           'button to be pushed...', co.Fore.BLUE + '(press '
+                           'Ctrl-C to abort)')
+                while not reboot_done.wait(update_interval):
+                    print('\r' + co.Fore.WHITE + next(waiting_indicator),
+                          *message, end='', file=sys.stderr)
+                else:
+                    print('\r' + co.Fore.MAGENTA + 'Teensy upload:',
+                          status.message if status.is_set()
+                          else co.Fore.RED + 'ERROR', 100 * ' ',
+                          file=sys.stderr)
+                wait_complete.set()
+
+            # Launch background thread to display status while waiting for
+            # programming button to be pressed.
+            thread = threading.Thread(target=wait_for_reboot)
+            thread.daemon = True
+            thread.start()
+            teensy_reboot_exe = teensy_tools_dir.joinpath('teensy_reboot.exe')
+
+            try:
+                # Keep retrying firmware upload until Ctrl-C is pressed.
+                while not wait_complete.is_set():
+                    return_code, stdout, stderr = \
+                        ch.with_loop(ch.run_command)(teensy_reboot_exe,
+                                                     shell=True, verbose=False)
+                    if return_code == 0:
+                        status.message = co.Fore.LIGHTGREEN_EX + 'SUCCESS'
+                        status.set()
+                        break
+            except KeyboardInterrupt:
+                status.message = co.Fore.YELLOW + 'CANCELLED'
+                status.set()
+            finally:
+                reboot_done.set()
+                wait_complete.wait(5)
+                print(co.Fore.RESET + co.Back.RESET + '', file=sys.stderr,
+                      end='')
+
+        # Trigger PlatformIO upload command, which will use graphical uploader
+        # that is already running.
         # XXX Requires PlatformIO package, `platformio-tool-teensy==1.21.0`.
-        upload_conda('dropbot')
+        # If upload fails, leave Teensy GUI open for 10 seconds to allow manual
+        # reset into programming mode.
+        upload_conda('dropbot', on_error=on_error)
+
         # Kill Teensy graphical uploader.
         p.kill()
     else:
