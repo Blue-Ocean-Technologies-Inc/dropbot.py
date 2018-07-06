d551e120323cba5c5d0e97b1e8d8c7e9a640eca3
Christian Fobel <christian@fobel.net>
2018-07-06 14:03:02 -0400

    feat(threshold): add sync target_capacitance
    Add `threshold.target_capacitance()` synchronous wrapper around
    `threshold.target_capacitance_event()`.  If target capacitance is not
    reached after a specified timeout, retry with 98% of the target
    capacitance.
    
    See #33.

--- dropbot/threshold.py
+++ dropbot/threshold.py
@@ -4,8 +4,12 @@
 from __future__ import division, print_function, unicode_literals
 import datetime as dt
 import threading
+import time
+import types
 
+from logging_helpers import _L
 import numpy as np
+import si_prefix as si
 
 
 def target_capacitance_event(self, channels, target_capacitance, count=3):
@@ -68,3 +72,61 @@ def target_capacitance_event(self, channels, target_capacitance, count=3):
     self.update_state(target_capacitance=target_capacitance, target_count=count)
 
     return threshold_reached
+
+
+def target_capacitance(self, channels, target_capacitance, timeout_s=1.5,
+                       retries=10, **kwargs):
+    '''
+    Synchronous wrapper around :func:`target_capacitance_event`.
+
+    If target capacitance not reached after specified timeout, retry with 98%
+    of the target capacitance.
+
+    Parameters
+    ----------
+    channels : list-like
+        Channels to actuate.
+    target_capacitance : float
+        Target capacitance value.
+    timeout_s : optional, float
+        Timeout (in seconds) before retrying with reduced (98%) target
+        capacitance.
+    retries : optional, int
+        Maximum number of retry attempts.
+
+    Returns
+    -------
+    `OrderedDict`
+        The ``capacitance-exceeded`` DropBot event message with the fields:
+
+            - ``start``: start timestamp.
+            - ``end``: end timestamp.
+            - ``timeout_s``: number of seconds to wait before retrying.
+            - ``retries``: number of retries before success.
+            - ``new_value``: capacitance reached.
+            - ``target``: target capacitance.
+            - ``V_a``: actuation voltage.
+
+    Raises
+    ------
+    RuntimeError
+        If target capacitance was not reached after maximum number of retries.
+    '''
+    for i in range(retries):
+        event = target_capacitance_event(self, channels, target_capacitance,
+                                         **kwargs)
+        if event.wait(timeout_s):
+            target_capacitance = .98 * event.message['new_value']
+            event.message['start'] = event.start
+            event.message['end'] = event.end
+            event.message['retries'] = i
+            event.message['timeout_s'] = timeout_s
+            return event.message
+        else:
+            target_capacitance *= .98
+            _L().debug('Timed out on attempt %d.  Reduce target capacitance to'
+                       ' %sF and retry.', i + 1,
+                       si.si_format(target_capacitance, 2))
+            event.cancel(None)
+    else:
+        raise RuntimeError('Timed out waiting for target capacitance')
