5c13e48e69254943f882d9ebe84a9eaa5df74b75
Christian Fobel <christian@fobel.net>
2018-07-13 10:08:48 -0400

    feat(signal-timer): add signal timer
    Add `Node::signal_timer_` member and update timer state with the current
    millisecond counter on every `loop()` iteration.
    
    Receiver callback functions may be connected to the signal timer, each to
    be called at the respective specified interval (millisecond resolution).
    By default, receiver functions are called repeatedly, once every interval,
    but may optionally only be called after the first interval has expired.

--- src/Node.h
+++ src/Node.h
@@ -49,6 +49,8 @@
 #include "drops.h"
 #include "format.h"
 #include "Time.h"
+#include "Signal.h"
+#include "SignalTimer.h"
 
 #define CPU_RESTART_ADDR (uint32_t *)0xE000ED0C
 #define CPU_RESTART_VAL 0x5FA0004
@@ -245,6 +247,8 @@ public:
   //
   // Time of most recent drops detection.
   uint32_t drops_timestamp_ms_;
+
+  SignalTimer signal_timer_ms_;
 
   Node() : BaseNode(),
            BaseNodeConfig<config_t>(dropbot_Config_fields),
@@ -656,6 +660,8 @@ public:
 
     // poll button state
     output_enable_input.process(now);
+
+    signal_timer_ms_.update(now);
 
     if (state_._.hv_output_enabled && state_._.hv_output_selected) {
       // High-voltage output is enabled and selected.
