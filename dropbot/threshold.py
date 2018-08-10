efda367f5690fd22d0285ffc5eee9ad5fb2b263f
Christian Fobel <christian@fobel.net>
2018-08-10 15:57:58 -0400

    feat(threshold): add target capacitance coroutine
    Add `co_target_capacitance` _coroutine_ to actuate the specified channels
    and wait until the target capacitance is reached.
    
    **Since the function is a _coroutine_ timeout behaviour can be achieved if
    necessary by cancelling the corresponding `asyncio` task.**

--- dropbot/threshold.py
+++ dropbot/threshold.py
@@ -4,15 +4,15 @@
 from __future__ import division, print_function, unicode_literals
 import datetime as dt
 import threading
-import time
 import types
+import uuid
 
-from logging_helpers import _L
+import base_node_rpc as bnr
+import base_node_rpc.async
 import dropbot as db
 import dropbot.proxy
-import numpy as np
 import pandas as pd
-import si_prefix as si
+import trollius as asyncio
 
 
 def actuate_channels(self, channels, timeout=None, allow_disabled=True):
@@ -82,8 +82,14 @@ def actuate_channels(self, channels, timeout=None, allow_disabled=True):
         return _actuate()
 
 
-def target_capacitance_event(self, channels, target_capacitance, count=3):
+@asyncio.coroutine
+def co_target_capacitance(self, channels, target_capacitance, count=3,
+                          **kwargs):
     '''
+    XXX Coroutine XXX
+
+    Actuate specified channels and wait until target capacitance is reached.
+
     Parameters
     ----------
     channels : list-like
@@ -95,111 +101,64 @@ def target_capacitance_event(self, channels, target_capacitance, count=3):
 
     Returns
     -------
-    threading.Event
-        Event that is set once target capacitance has been reached.  The
-        following attributes are added to the event as well:
-
-         - ``cancel()``: method to cancel threshold request.
-         - ``start``: time channels were actuated.
-         - ``end``: time target capacitance was reached.
-         - ``message``: the ``capacitance-exceeded`` DropBot event message.
+    dict
+        ``capacitance-exceeded`` DropBot event message with the following
+        additional fields:
+         - ``start``: time channels were actuated (`datetime.datetime`).
+         - ``end``: time target capacitance was reached (`datetime.datetime`).
+         - ``actuated_channels``: actuated channels (`list`).
     '''
-    state_of_channels = np.zeros(self.number_of_channels, dtype=int)
-    state_of_channels[channels] = 1
-    self.state_of_channels = state_of_channels
-    # XXX TODO Should probably wait until channels are reported as actuated
-    # through `channels-updated` event.
+    actuation_uuid1 = uuid.uuid1()
+    with self.transaction_lock:
+        capacitance_messages = []
 
-    threshold_reached = threading.Event()
+        def _on_capacitance(message):
+            message['actuation_uuid1'] = actuation_uuid1
+            message['actuated_channels'] = actuated_channels
+            capacitance_messages.append(message)
 
-    # Add event to allow caller to cancel.
-    threshold_reached.start = dt.datetime.now()
+        def _on_done(message):
+            message['end'] = dt.datetime.now()
+            message['start'] = start
+            message['actuated_channels'] = actuated_channels
+            threshold_reached.result = message
+            self.signals.signal('capacitance-exceeded').disconnect(_on_done)
+            loop.call_soon_threadsafe(threshold_reached.set)
 
-    def _on_done(message):
-        threshold_reached.end = dt.datetime.now()
-        threshold_reached.message = message
-        # capacitance = message['new_value']
-        self.signals.signal('capacitance-exceeded').disconnect(_on_done)
-        threshold_reached.set()
+        threshold_reached = asyncio.Event()
+        loop = bnr.async.ensure_event_loop()
 
-    # Attach cancel method to event object.
-    def _cancel(event):
-        '''
-        Disable DropBot ``capacitance-exceeded`` monitoring and disconnect
-        event callback.
-        '''
-        self.update_state(target_capacitance=0)
-        self.signals.signal('capacitance-exceeded').disconnect(_on_done)
+        # Perform actuation and wait until actuation has been applied.
+        actuated_channels = actuate_channels(self, channels, **kwargs)
 
-    threshold_reached.cancel = _cancel
+        # Connect to capacitance exceeded DropBot events, i.e., when specified
+        # target capacitance has been exceeded.
+        self.signals.signal('capacitance-exceeded').connect(_on_done)
 
-    # Connect to capacitance exceeded DropBot events, i.e., when specified
-    # target capacitance has been exceeded.
-    self.signals.signal('capacitance-exceeded').connect(_on_done, weak=False)
-    # Set `target_capacitance` to non-zero value to enable DropBot
-    # `capacitance-exceeded` event once target capacitance is reached and
-    # sustained for `target_count` consecutive readings.
-    self.update_state(target_capacitance=target_capacitance, target_count=count)
+        # Record timestamp where actuation has been verified as applied.
+        start = dt.datetime.now()
 
-    return threshold_reached
+        # Connect to `capacitance-updated` signal to record capacitance values
+        # measured during actuation.
+        self.signals.signal('capacitance-updated').connect(_on_capacitance)
 
+        # Set `target_capacitance` to non-zero value to enable DropBot
+        # `capacitance-exceeded` event once target capacitance is reached and
+        # sustained for `target_count` consecutive readings.
+        self.update_state(target_capacitance=target_capacitance,
+                          target_count=count)
 
-def target_capacitance(self, channels, target_capacitance, timeout_s=1.5,
-                       retries=10, **kwargs):
-    '''
-    Synchronous wrapper around :func:`target_capacitance_event`.
-
-    If target capacitance not reached after specified timeout, retry with 98%
-    of the target capacitance.
-
-    Parameters
-    ----------
-    channels : list-like
-        Channels to actuate.
-    target_capacitance : float
-        Target capacitance value.
-    timeout_s : optional, float
-        Timeout (in seconds) before retrying with reduced (98%) target
-        capacitance.
-    retries : optional, int
-        Maximum number of retry attempts.
-
-    Returns
-    -------
-    `OrderedDict`
-        The ``capacitance-exceeded`` DropBot event message with the fields:
-
-            - ``start``: start timestamp.
-            - ``end``: end timestamp.
-            - ``timeout_s``: number of seconds to wait before retrying.
-            - ``retries``: number of retries before success.
-            - ``new_value``: capacitance reached.
-            - ``target``: target capacitance.
-            - ``V_a``: actuation voltage.
-
-    Raises
-    ------
-    RuntimeError
-        If target capacitance was not reached after maximum number of retries.
-    '''
-    for i in range(retries):
-        event = target_capacitance_event(self, channels, target_capacitance,
-                                         **kwargs)
-        if event.wait(timeout_s):
-            target_capacitance = .98 * event.message['new_value']
-            event.message['start'] = event.start
-            event.message['end'] = event.end
-            event.message['retries'] = i
-            event.message['timeout_s'] = timeout_s
-            return event.message
-        else:
-            target_capacitance *= .98
-            _L().debug('Timed out on attempt %d.  Reduce target capacitance to'
-                       ' %sF and retry.', i + 1,
-                       si.si_format(target_capacitance, 2))
-            event.cancel(None)
-    else:
-        raise RuntimeError('Timed out waiting for target capacitance')
+        try:
+            yield asyncio.From(threshold_reached.wait())
+            # Attach list of capacitance update messages recorded during
+            # actuation to result.
+            threshold_reached.result['capacitance_updates'] = \
+                capacitance_messages
+            raise asyncio.Return(threshold_reached.result)
+        finally:
+            self.signals.signal('capacitance-updated')\
+                .disconnect(_on_capacitance)
+            self.signals.signal('capacitance-exceeded').disconnect(_on_done)
 
 
 def execute_actuation(self, chip_info_, specific_capacitance, channels,
